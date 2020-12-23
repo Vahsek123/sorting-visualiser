dc221dbed19d910898e850cbaa4de3e139c782d0
Vahsek123 <68935747+Vahsek123@users.noreply.github.com>
2020-12-23 19:29:25 +1300

    Reset project to now use webpack

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
+/// <reference path="../node_modules/@types/p5/global.d.ts" />
+import * as P5 from "p5";
+
 console.log("Start of the visualiser");
+
+var sketch = (p5: P5) => {
+    p5.setup = () => {
+        const canvas = p5.createCanvas(600, 400);
+    }
+
+    p5.draw = () => {
+        p5.background(245);
+        p5.ellipse(50, 50, 80, 80);
+    }
+}
+
+new P5(sketch)
